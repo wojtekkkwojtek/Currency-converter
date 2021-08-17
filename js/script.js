e8854051226ffcb38f19121e2d483ec54545b7b9
Wojciech K <wojtekkk@protonmail.com>
2021-08-17 18:28:50 +0200

    functions renamed N unnecessary variables removed

--- js/script.js
+++ js/script.js
@@ -1,38 +1,40 @@
 {
-    const getResult = (amount, currency) => {
+    const calculateResult = (amount, currency) => {
         const rateUSD = 3.1;
         const rateEUR = 4.1;
         const rateGBP = 5.1;
 
-    switch(currency) {
-        case "EUR":
-            return amount * rateEUR;
-        case "GBP":
-            return amount * rateGBP;
-        case "USD":
-            return amount * rateUSD;
+        switch (currency) {
+            case "EUR":
+                return amount * rateEUR;
+            case "GBP":
+                return amount * rateGBP;
+            case "USD":
+                return amount * rateUSD;
         }
     };
 
-    const updateResultText = (amount, result, currency) => {
+    const updateResultText = (result) => {
         const resultElement = document.querySelector(".js-result");
-        resultElement.innerHTML = result.toFixed(2)};
+        resultElement.innerHTML = result.toFixed(2)
+    };
 
     const onFormSubmit = (event) => {
         event.preventDefault();
-    
         const amountElement = document.querySelector(".js-amount");
         const currencyElement = document.querySelector(".js-currency");
         const amount = +amountElement.value;
         const currency = currencyElement.value;
-        const result = getResult(amount, currency);
-        updateResultText(amount, result, currency);
-};
+        const result = calculateResult(amount, currency);
+        updateResultText(result);
+
+    };
 
     const init = () => {
         const formElement = document.querySelector(".js-form");
         formElement.addEventListener("submit", onFormSubmit);
-};
+    };
 
-    init ();
+    init();
 }
+
